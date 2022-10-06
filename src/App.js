21d3a8e2ec90a8092abea4af9f7747102a53c06b
fail2ban1337 <ayoubebelomari@gmail.com>
2022-10-06 19:47:03 +0100

    feat: add  CartHandleContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React, { createContext, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, rootTheme } from "./components/Theme";
 import Header from "./containers/Header";
 import HomePage from "./containers/Home";
+export const CartHandleContext = createContext();
 
 function App() {
+  const [cart, setCart] = useState({});
   return (
     <ThemeProvider theme={rootTheme}>
       <GlobalStyle />
-      <Header />
-      <HomePage />
+      <CartHandleContext.Provider value={{ cart, setCart }}>
+        <Header />
+        <HomePage />
+      </CartHandleContext.Provider>
     </ThemeProvider>
   );
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Container, ImageSection, PorductInfo, RowContainer } from "./styles";
-
+import { CartHandleContext } from "../../App";
 export default function Home({ children, ...restProps }) {
   return <Container>{children}</Container>;
 }
@@ -23,11 +23,11 @@ Home.ImageSection = function ({ children, ...restProps }) {
             <img
               className="image_thumb"
               src="images/image-product-2-thumbnail.jpg"
-            />{" "}
+            />
             <img
               className="image_thumb"
               src="images/image-product-3-thumbnail.jpg"
-            />{" "}
+            />
             <img
               className="image_thumb"
               src="images/image-product-4-thumbnail.jpg"
@@ -39,23 +39,42 @@ Home.ImageSection = function ({ children, ...restProps }) {
   );
 };
 
-Home.ProductInof = function ({ children }) {
+Home.ProductInof = function HomeProductInof({ ...restProps }) {
+  const { cart, setCart } = useContext(CartHandleContext);
+  const productData = {
+    productCat: "NEAKER COMPANY",
+    productName: "Fall Limited Edition Sneakers",
+    productDesc: `Thes low-profile sneakers are your perfect casual wear companion,
+    Feautring a durable rubber outer solde, they'll withstand the
+    everything the weather can offer.`,
+    productImg: "images/image-product-1-thumbnail.jpg",
+    productOldPrice: 250.0,
+    productPrice: 125.0,
+  };
+
+  const [prdTotal, SetProdTotal] = useState(0);
+
+  const HandleAddToCart = (e) => {
+    e.preventDefault();
+    if (prdTotal > 0) {
+      setCart({
+        products: { ...productData, prdTotal },
+      });
+    }
+  };
+
   return (
     <PorductInfo>
       <div className="productSectionCol">
-        <h3>SNEAKER COMPANY</h3>
-        <h2>Fall Limited Edition Sneakers</h2>
-        <span className="productDesc">
-          Thes low-profile sneakers are your perfect casual wear companion,
-          Feautring a durable rubber outer solde, they'll withstand the
-          everything the weather can offer.
-        </span>
+        <h3>{productData.productCat}</h3>
+        <h2>{productData.productName}</h2>
+        <span className="productDesc">{productData.productDesc}</span>
         <div className="price_container">
           <div className="price_containerRow">
-            <span className="priceValue">$125.00</span>
+            <span className="priceValue">${productData.productPrice}</span>
             <span className="discount">50%</span>
           </div>
-          <span className="oldPrice">$250.00</span>
+          <span className="oldPrice">${productData.productOldPrice}</span>
         </div>
         <div className="btnRow">
           <div className="add_product_number">
@@ -64,6 +83,9 @@ Home.ProductInof = function ({ children }) {
               height="4"
               xmlns="http://www.w3.org/2000/svg"
               link="http://www.w3.org/1999/xlink"
+              onClick={() =>
+                SetProdTotal((prevValue) => (prevValue > 0 ? prevValue - 1 : 0))
+              }
             >
               <defs>
                 <path
@@ -73,12 +95,13 @@ Home.ProductInof = function ({ children }) {
               </defs>
               <use fill="#FF7E1B" fillRule="nonzero" href="#a" />
             </svg>
-            <span className="add_product_number_value">0</span>
+            <span className="add_product_number_value">{prdTotal}</span>
             <svg
               width="12"
               height="12"
               xmlns="http://www.w3.org/2000/svg"
               link="http://www.w3.org/1999/xlink"
+              onClick={() => SetProdTotal((prevValue) => prevValue + 1)}
             >
               <defs>
                 <path
@@ -89,7 +112,10 @@ Home.ProductInof = function ({ children }) {
               <use fill="#FF7E1B" fillRule="nonzero" href="#b" />
             </svg>
           </div>
-          <div className="add_productToCard">
+          <div
+            className="add_productToCard"
+            onClick={(e) => HandleAddToCart(e)}
+          >
             <svg className="add_productToCardicon" width="22" height="20">
               <path
                 d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
